6953ef124027dee462963d3a80179ebfc1813358
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2021-09-20 20:23:14 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -71,6 +71,7 @@ const Rank = ({ athletes, athletesM, athletesF }) => {
             <Th>Footloose</Th>
             <Th>Dúzia</Th>
             <Th>The Cub Complex</Th>
+            <Th>Penrose</Th>
           </Tr>
         </Thead>
         <Tbody>
@@ -84,6 +85,7 @@ const Rank = ({ athletes, athletesM, athletesF }) => {
               <Td>{athlete.results[0] || '-'}</Td>
               <Td>{athlete.results[1] || '-'}</Td>
               <Td>{athlete.results[2] || '-'}</Td>
+              <Td>{athlete.results[3] || '-'}</Td>
             </Tr>
           ))}
         </Tbody>
@@ -148,6 +150,13 @@ const sortProva3 = (a, b) => {
   return v2 - v1;
 };
 
+const sortProva4 = (a, b) => {
+  const v1 = (a.results[0] || '1000000').replace('CAP+', '100').replace(':', '');
+  const v2 = (b.results[0] || '1000000').replace('CAP+', '100').replace(':', '');
+
+  return v1 - v2;
+};
+
 const rank = (arr) => {
   return arr
     .map((a) => ({ ...a, pontos: 0 }))
@@ -156,6 +165,8 @@ const rank = (arr) => {
     .sort(sortProva2)
     .map((a, i) => ({ ...a, pontos: a.pontos + i + 1 }))
     .sort(sortProva3)
+    .map((a, i) => ({ ...a, pontos: a.pontos + i + 1 }))
+    .sort(sortProva4)
     .map((a, i) => ({ ...a, pontos: a.pontos + i + 1 }))
     .sort(sortPontos);
 };
@@ -185,12 +196,12 @@ export async function getStaticProps() {
   const leonardo = a40m.find((a) => a.name === 'LEONARDO MESQUITA DA CRUZ');
 
   fabio.results = ['CAP+110', ...fabio.results];
-  danilo.results = ['CAP+57', 123, 84, ...danilo.results];
+  danilo.results = ['CAP+57', 123, 84, 'CAP+9', ...danilo.results];
 
   const [renanProva1, ...renanResults] = renan.results;
-  renan.results = [renanProva1, 126, 20.5, ...renanResults];
+  renan.results = [renanProva1, 126, 20.5, 'CAP+9', ...renanResults];
 
-  leonardo.results = ['CAP+97', 123, 65, ...leonardo.results];
+  leonardo.results = ['CAP+97', 123, 65, 'CAP+6', ...leonardo.results];
   juliana.results = ['CAP+170', ...juliana.results];
 
   const athletesM = rank(reduceWeight(0.62, [...eliteM, ...a40m, ...a45m]));
